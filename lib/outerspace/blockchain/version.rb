dc45beeeb3bce634af561e35b826a33d5fe43fb8
JesusGautamah <jesusgautamah@ghost-in-the-sh.localdomain>
2022-10-18 12:24:50 -0300

    init gem package and reorginize license

---
+++ lib/outerspace/blockchain/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Outerspace
+  module Blockchain
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/outerspace/blockchain_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Outerspace::Blockchain do
+  it "has a version number" do
+    expect(Outerspace::Blockchain::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
