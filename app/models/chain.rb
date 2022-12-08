dfc7101f078e40a21d02787e04ab01392261e300
JesusGautamah <lima.jesuscc@gmail.com>
2022-12-08 04:56:47 -0300

    hotfix: seeds and added env variables

--- app/models/chain.rb
+++ app/models/chain.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Chain < ApplicationRecord
-  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 40 }
   validates :maintainer, presence: true, length: { maximum: 50 }
   validates :chain_version, presence: true, length: { maximum: 20 }
   validates :description, presence: true, length: { maximum: 1000 }
 
   has_many :blocks, dependent: :destroy
 end
+
