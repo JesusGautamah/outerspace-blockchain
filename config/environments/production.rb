d0ddcdae8eee23b6c91bad441cd48547d242e241
JesusGautamah <lima.jesuscc@gmail.com>
2023-01-31 22:27:10 +0000

    add force ssl to env variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # force ssl
+  config.force_ssl = ENV["FORCE_SSL"] == "true"
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
